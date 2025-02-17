4d081f60e99371cb9ea2ecde0f8d76150829cff2
gilangaryo <gilang91668@gmail.com>
2025-02-17 19:35:17 +0700

    fix: bug decimal

--- contracts/FactoryExchange.sol
+++ contracts/FactoryExchange.sol
@@ -5,7 +5,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FactoryExchange is Ownable {
-    uint256 public constant TOKEN_PER_IDLE = 150000; // 1 IDLE = 150000 token AI
+    uint256 public constant TOKEN_PER_IDLE = 20; // 1 IDLE = 20 token AI
+    uint256 public constant MAX_SUPPLY_AI = 30000 * 10 ** 18; // Maksimal 30,000 token AI
     IERC20 public idleToken;
     address[] public allTokens;
 
@@ -25,6 +26,13 @@ contract FactoryExchange is Ownable {
         uint256 totalSupply;
         address owner;
         uint256 createdAt;
+        string iconUrl;
+        string description;
+        string behaviour;
+        uint256 openPrice;
+        uint256 highPrice;
+        uint256 lowPrice;
+        uint256 closePrice;
     }
 
     mapping(address => TokenInfo) public tokenDetails;
@@ -32,23 +40,42 @@ contract FactoryExchange is Ownable {
     function createToken(
         string memory name,
         string memory symbol,
-        uint256 idleAmount
+        uint256 idleAmount,
+        string memory iconUrl,
+        string memory description,
+        string memory behaviour
     ) public {
         require(idleAmount > 0, "IDLE amount must be greater than zero");
 
-        // Transfer IDLE dari user ke kontrak sebagai reserve awal
         idleToken.transferFrom(msg.sender, address(this), idleAmount);
 
-        // Buat token AI
         AIToken newToken = new AIToken(name, symbol, address(this));
 
-        // Simpan info token baru, supply = 0
+        uint256 initialPrice = 0.000001 ether;
+
+        uint256 tokenAmount = calculateTokensForIDLE(address(newToken), idleAmount);
+        AIToken(address(newToken)).mint(msg.sender, tokenAmount);
+
         userTokens[msg.sender] = address(newToken);
-        tokenDetails[address(newToken)] = TokenInfo(name, symbol, 0, msg.sender, block.timestamp);
+        tokenDetails[address(newToken)] = TokenInfo(
+            name,
+            symbol,
+            tokenAmount / 10**18,
+            msg.sender,
+            block.timestamp,
+            iconUrl,
+            description,
+            behaviour,
+            initialPrice,
+            initialPrice,
+            initialPrice,
+            initialPrice
+        );
         allTokens.push(address(newToken));
 
         emit TokenCreated(msg.sender, address(newToken), block.timestamp);
     }
+
 
     function buyToken(address token, uint256 idleAmount) public {
         require(idleAmount > 0, "IDLE amount must be greater than zero");
@@ -57,12 +84,16 @@ contract FactoryExchange is Ownable {
         require(tokenInfo.owner != address(0), "Token does not exist");
 
         uint256 tokenAmount = calculateTokensForIDLE(token, idleAmount);
+        require(tokenInfo.totalSupply + tokenAmount <= MAX_SUPPLY_AI, "Max supply reached");
 
         idleToken.transferFrom(msg.sender, address(this), idleAmount);
 
         AIToken(token).mint(msg.sender, tokenAmount);
 
-        tokenInfo.totalSupply += tokenAmount / 10**18;
+        tokenInfo.totalSupply += tokenAmount;
+
+        uint256 currentPrice = getCurrentPrice(token);
+        updateOHLC(token, currentPrice);
 
         emit TokenPurchased(msg.sender, token, tokenAmount);
     }
@@ -79,23 +110,26 @@ contract FactoryExchange is Ownable {
         AIToken(token).transferFrom(msg.sender, address(this), tokenAmount);
         AIToken(token).burn(tokenAmount);
 
-        tokenInfo.totalSupply -= tokenAmount / 10**18;
+        tokenInfo.totalSupply -= tokenAmount;
 
         idleToken.transfer(msg.sender, idleAmount);
+
+        uint256 currentPrice = getCurrentPrice(token);
+        updateOHLC(token, currentPrice);
 
         emit TokenSold(msg.sender, token, tokenAmount, idleAmount);
     }
 
     function getCurrentPrice(address token) public view returns (uint256) {
         TokenInfo storage tokenInfo = tokenDetails[token];
-        uint256 supply = tokenInfo.totalSupply;
+        uint256 supply = tokenInfo.totalSupply / 10 ** 18;
 
-        return 0.00003 ether + ((supply * 0.00003 ether) / 100000);
+        return 0.000001 ether + ((supply * 0.000001 ether) / 100000);
     }
 
     function calculateTokensForIDLE(address token, uint256 idleAmount) public view returns (uint256) {
         uint256 pricePerToken = getCurrentPrice(token);
-        return (idleAmount * 10**18) / pricePerToken; 
+        return (idleAmount * 10**18) / pricePerToken;
     }
 
     function calculateIDLEForTokens(address token, uint256 tokenAmount) public view returns (uint256) {
@@ -109,6 +143,28 @@ contract FactoryExchange is Ownable {
 
     function getAllTokens() public view returns (address[] memory) {
         return allTokens;
+    }
+
+    function getMarketCap(address token) public view returns (uint256) {
+        TokenInfo storage tokenInfo = tokenDetails[token];
+        require(tokenInfo.owner != address(0), "Token does not exist");
+
+        uint256 currentPrice = getCurrentPrice(token);
+        uint256 marketCap = tokenInfo.totalSupply * currentPrice / 10**18;
+
+        return marketCap;
+    }
+
+    function updateOHLC(address token, uint256 currentPrice) internal {
+        TokenInfo storage tokenInfo = tokenDetails[token];
+
+        if (currentPrice > tokenInfo.highPrice) {
+            tokenInfo.highPrice = currentPrice;
+        }
+        if (currentPrice < tokenInfo.lowPrice) {
+            tokenInfo.lowPrice = currentPrice;
+        }
+        tokenInfo.closePrice = currentPrice;
     }
 }
 
